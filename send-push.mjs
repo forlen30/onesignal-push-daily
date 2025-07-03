a3b622b57dc3b05383f908bc0a5719f0a6ce3ce5
forlen30 <sdforlen@gmail.com>
2025-07-04 00:44:12 +0700

    Update and rename send-push.js to send-push.mjs

--- send-push.mjs
+++ send-push.mjs
@@ -1,5 +1,3 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
 const ONESIGNAL_APP_ID = '68a7a06b-4814-4d41-987a-f14c5631c5d5'; // <<< ของคุณ
 const ONESIGNAL_API_KEY = 'os_v2_app_nct2a22icrgudgd26fgfmmof2u4zcrzd22zuwwvc3oxv2265hqu4ylndlpvhgq3eeq42ntpwiea2e23lonnbn5gadddmsc4pzvqm3va'; // <<< ของคุณ
 
@@ -9,7 +7,7 @@ async function sendNotification() {
     included_segments: ['All'],
     headings: { "en": "ไพ่หนุ่มฟูพร้อมสุ่มแล้ว!" },
     contents: { "en": "วันนี้สามารถเปิดไพ่หนุ่มฟูได้อีกครั้งแล้ว คลิกเพื่อเริ่มวันใหม่เลยนะคะ 🌈✨" },
-    url: 'https://my-familiars-v2.netlify.app/', // ลิงก์หน้าเว็บของคุณ
+    url: 'https://my-familiars-v2.netlify.app/',
   };
 
   const res = await fetch('https://onesignal.com/api/v1/notifications', {
